5e392cd083ca7a38bf0b8d0b018618373f2054e2
Randi Cagle <randic@ksu.edu>
2015-05-28 18:18:18 +0000

    Updated and complete assignment 27 CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
  require 'faker'
- 
+
+Post.find_or_create_by!(title: "Custom Post")
+Comment.find_or_create_by!(title: "Custom Post")
+
  # Create Posts
  50.times do
    Post.create!(
@@ -8,21 +11,13 @@
    )
  end
  posts = Post.all
- 
+
+
  # Create Comments
  100.times do
    Comment.create!(
      post: posts.sample,
      body: Faker::Lorem.paragraph
-   )
- end
-
-# Create Advertisements
-50.times do
-  Advertisement.create!(
-    title: Faker::Lorem.sentence,
-     copy: Faker::Lorem.paragraph,
-    price: rand * 10,
    )
  end
  
