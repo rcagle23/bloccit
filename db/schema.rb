889dbaf0a74b2832518a22d9199e1bb636e374f3
rcagle23 <randic@ksu.edu>
2015-07-22 17:47:11 +0000

    Checkpoint 39 Authorization complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721155504) do
+ActiveRecord::Schema.define(version: 20150722164022) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150721155504) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
