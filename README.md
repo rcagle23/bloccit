4ab8bc9eead911c2eea3e7712cdf12cec1be16cd
rcagle23 <randic@ksu.edu>
2015-07-23 14:29:39 +0000

    Progress on assignment 39, authorization

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Bloccit
+<h2>Bloccit</h2>
 Reddit replica to teach the fundamentals of web development and Rails.
 
 <a href="https://radiant-reaches-6831.herokuapp.com" target="_blank">View the app</a> with Heroku
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,14 @@ class PostPolicy < ApplicationPolicy
     def index?
         true
     end
+    
+    class Scope < Scope
+        def resolve
+            if user.present? && (user.admin? || user.moderator?)
+                scope.all
+            else
+                scope.where(:published => true)
+            end
+        end
+    end
 end
