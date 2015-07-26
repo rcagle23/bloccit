99e014ab8c8befd8781e7c633402d742711af9c9
rcagle23 <randic@ksu.edu>
2015-07-26 15:42:24 +0000

    Checkpoint 41 Interlude Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-   resources :topics do
-     resources :posts, except: [:index]
-   end
+  resources :users, only: [:update]
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
    
   get 'about' => 'welcome#about'
   
