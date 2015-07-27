a4f1b0de06312e14208280efd41bc9d0bc332851
rcagle23 <randic@ksu.edu>
2015-07-27 16:15:47 +0000

    progress on topics and posts assignment

--- db/migrate/20150726151350_create_summaries.rb
+++ db/migrate/20150726151350_create_summaries.rb
@@ -1,10 +1,9 @@
 class CreateSummaries < ActiveRecord::Migration
   def change
-    create_table :summaries do |t|
-      t.string :name
-      t.boolean :public
-      t.text :description
 
+    create_table :summaries do |t|
+      t.text :description
+      t.references :post_id, index:true, foreign_key:true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150726232953) do
+ActiveRecord::Schema.define(version: 20150726151350) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -29,20 +29,19 @@ ActiveRecord::Schema.define(version: 20150726232953) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
-    t.integer  "summary_id"
   end
 
-  add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "summaries", force: true do |t|
-    t.string   "name"
-    t.boolean  "public"
     t.text     "description"
+    t.integer  "post_id_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "summaries", ["post_id_id"], name: "index_summaries_on_post_id_id"
 
   create_table "topics", force: true do |t|
     t.string   "name"
