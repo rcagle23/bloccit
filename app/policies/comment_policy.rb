fe455012da8cfcf8ae3cec8495857d9f40dd4ae8
rcagle23 <randic@ksu.edu>
2015-08-03 03:20:38 +0000

    assignment 46 comments progress

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,13 @@
+class CommentPolicy < ApplicationPolicy
+    def new?
+        create?
+    end
+    
+    def create?
+        user.present?
+    end
+    
+    def show?
+        true
+    end
+end
