7a4c2fe6cce3485364147aeb972ed4726f3e8752
rcagle23 <randic@ksu.edu>
2015-08-05 00:53:11 +0000

    assignment 48 intro to rspec complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,36 @@
-describe Vote do
+require 'rails_helper'
+
+describe 'Vote' do
   describe "validations" do
+    
     describe "value validation" do
+      before do
+        @user = User.create!(email: 'usernamehere@example.com', password: 'helloworld')
+        @topic = Topic.create!(name: 'Topics Title')
+        @post = Post.create!(title: 'post title', body: 'post body needs to be at least 20 characters', topic: @topic, user: @user)
+        
+      end
       it "only allows -1 or 1 as values" do
         # your expectations here
+        vote = @post.votes.create(value: -1)
+        expect(vote.value).to eq(-1)
+        expect(vote.valid?).to_be true
+      end
+      
+       it "only allows -1 or 1 as values" do
+        # your expectations here
+        vote = @post.votes.create(value: 1)
+        expect(vote.value).to eq(1)
+        expect(vote.valid?).to_be true
+
+      end
+      
+      it "only should reject values other than -1 or 1" do
+        # your expectations here
+        vote = @post.votes.create(value: 4)
+        expect(vote.valid?).to_be false
       end
     end
+    
   end
 end
