53f408b3f4702669f4d2db088756be4d0ea7d07d
rcagle23 <randic@ksu.edu>
2015-08-10 15:54:35 +0000

    assignment 51 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,12 +10,12 @@ describe User do
         end
         
         it "returns 'nil' if the user has not favorited the post" do
-            expect(@user.favorite(@post)).to eq(nil)
+            expect(@user.favorited(@post)).to eq(nil)
         end
         
         it "returns the appropriate favorite if it exists" do
-            @favorite = @user.favorites.find(@post)
-            expect(@user.favorite(@post)).to eq(@post)
+            @favorite = @user.favorites.create(post: @post)
+            expect(@user.favorited(@post)).to eq(@favorite)
         end
     end
 end
